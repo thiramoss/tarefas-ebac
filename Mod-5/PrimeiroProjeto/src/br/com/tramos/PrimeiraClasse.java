572bebb92276481ec74e90c19b38f826abfcb2e6
thiramoss <trs.tiba@gmail.com>
2024-04-29 13:01:20 -0300

    Módulo 5

--- Mod-5/PrimeiroProjeto/src/br/com/tramos/PrimeiraClasse.java
+++ Mod-5/PrimeiroProjeto/src/br/com/tramos/PrimeiraClasse.java
@@ -4,7 +4,7 @@ import java.awt.desktop.SystemSleepListener;
 
 public class PrimeiraClasse {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("Hello Thiago");
     }
 }
