fd03b351c35ccc5062c3f693adb988b9594c8022
Thiago Ramos <trs.tiba@gmail.com>
2024-04-29 12:31:32 -0300

    Modulo 5

---
+++ Mod-5/PrimeiroProjeto/src/br/com/tramos/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.tramos;
+
+import java.awt.desktop.SystemSleepListener;
+
+public class PrimeiraClasse {
+
+    public static void main(String args[]){
+        System.out.println("Hello Thiago");
+    }
+}
